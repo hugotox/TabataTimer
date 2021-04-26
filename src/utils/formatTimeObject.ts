59a3c588daf0d6ba3ab23b2ba29cb8cf54d71810
Hugo Pineda <hpineda83@gmail.com>
2021-04-25 21:41:01 -0700

    format time options to show time parts

--- src/utils/formatTimeObject.ts
+++ src/utils/formatTimeObject.ts
@@ -1,11 +1,23 @@
-export const formatTimeObject = (time: {
-  hours: number
-  minutes: number
-  seconds: number
-}) => {
+export const formatTimeObject = (
+  time: {
+    hours: number
+    minutes: number
+    seconds: number
+  },
+  includeHours: boolean = false,
+  includeMinutes: boolean = false,
+  includeSeconds: boolean = true
+) => {
   const { hours, minutes, seconds } = time
-  return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(
-    2,
-    '0'
-  )}:${String(seconds).padStart(2, '0')}`
+  const items = []
+  if (includeHours) {
+    items.push(String(hours).padStart(2, '0'))
+  }
+  if (includeMinutes) {
+    items.push(String(minutes).padStart(2, '0'))
+  }
+  if (includeSeconds) {
+    items.push(String(seconds).padStart(2, '0'))
+  }
+  return items.join(':')
 }
